52992684124e9df91f8bc0f3fa537b3897e9633d
Iván Gilberto Solis Pino <contacto@contacto.mx>
2026-02-09 19:11:51 -0600

    Nuevo Cambio SMS 3

--- backend/services/sms.js
+++ backend/services/sms.js
@@ -3,17 +3,20 @@ const telnyx = Telnyx(process.env.TELNYX_API_KEY);
 
 async function enviarSMS(celular, codigo) {
   try {
-    const response = await telnyx.messages.create({
+    const response = await telnyx.messages.send({
       messaging_profile_id: process.env.TELNYX_PROFILE_ID,
-      from: "SV",
+      from: "sv", // EXACTO al remitente
       to: `+52${celular}`,
       text: `Codigo Sociedad Valiente: ${codigo}`,
     });
 
-    console.log("📩 SMS enviado (Telnyx):", response.id);
+    console.log("📩 SMS enviado (Telnyx):", response.data.id);
     return true;
   } catch (error) {
-    console.error("❌ Error SMS Telnyx:", error);
+    console.error(
+      "❌ Error SMS Telnyx:",
+      error.response?.data || error.message || error
+    );
     return false;
   }
 }
